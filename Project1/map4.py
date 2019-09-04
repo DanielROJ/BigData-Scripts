d5df06d00175c48c82349f2842e03d02f1aeb2c3
Daniel Rojas <daniel-rojas2016@hotmail.com>
2019-09-04 14:04:14 -0500

    correccion en reducer 4

--- Project1/map4.py
+++ Project1/map4.py
@@ -3,12 +3,16 @@ import sys
 import re
 
 def mapping():
-    y = 0 ;
+    y = 0
+    x= 0;
     for i in sys.stdin:
         if(y==1):
             tmp = i.split(',');
             print("{},{},{}".format(tmp[1].strip(), tmp[6].strip(),tmp[2].strip().split('-')[0]))
         else:
             y=1;
+        x+=1;
+        if(x==10000):
+            break
 
 mapping();
--- Project1/reducer4.py
+++ Project1/reducer4.py
@@ -5,7 +5,7 @@ def reducer():
     for i in sys.stdin:
         tmp = i.strip().split(',');
         if(tmp[1] == "STAMFORD" and tmp[2] == "2015"):
-            print(tmp[0]);
+            print(int(tmp[0]));
             pass
 
 reducer();
