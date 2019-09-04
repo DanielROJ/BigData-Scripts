650bc1e55e542d18e17c4f46dcb84dfd3e716483
Daniel Rojas <daniel-rojas2016@hotmail.com>
2019-09-04 14:05:10 -0500

    correccion en reducer 4 2.0

--- Project1/map4.py
+++ Project1/map4.py
@@ -4,15 +4,12 @@ import re
 
 def mapping():
     y = 0
-    x= 0;
+
     for i in sys.stdin:
         if(y==1):
             tmp = i.split(',');
             print("{},{},{}".format(tmp[1].strip(), tmp[6].strip(),tmp[2].strip().split('-')[0]))
         else:
             y=1;
-        x+=1;
-        if(x==10000):
-            break
 
 mapping();
