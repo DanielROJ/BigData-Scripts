ec391bd6e03d025e2b9da31e02100a746001e769
Daniel Rojas <daniel-rojas2016@hotmail.com>
2019-09-18 00:13:49 -0500

    Mitad del taller echo

--- Dataframes_Taller/Puntto5_DATAFRAME.py
+++ Dataframes_Taller/Puntto5_DATAFRAME.py
@@ -9,4 +9,10 @@ spark = SparkSession.builder.appName('ABC').getOrCreate();
 
 df = spark.read.csv("price_paid_records.csv", header = True);
 
-dfOrder = df.select('Town/City','Price').groupBy('Town/City','Price').min().show();
+df1= df.select(f.split('Date of Transfer','-')[0].alias('year'),f.split('Date of Transfer','-')[1].alias('month'));
+
+df2 = df1.select('year','month').groupBy('year','month').count().sort(f.desc('count'));
+
+df3 = df2.dropDuplicates(['year']).select('year','month').sort('year').show(20);
+
+
