927366e147e8e16664ea0045b8d9685ace1a31a6
Daniel Rojas <daniel-rojas2016@hotmail.com>
2019-11-13 14:32:52 -0500

    update scraping

--- SCRAPPING TIEMPO/main.py
+++ SCRAPPING TIEMPO/main.py
@@ -1,13 +1,37 @@
 import urllib.request
 from bs4 import  BeautifulSoup;
+import re;
 import json;
 
 contents = urllib.request.urlopen("https://www.eltiempo.com/").read().decode();
 
 soup = BeautifulSoup(contents,'html.parser');
-titular = soup.find("div",class_= "article-details informacion");
-subTitulares = soup.find_all("div",class_= "article-details");
+listaArticulos = [];
 
-for x in subTitulares:
-    print(x);
+jsonLista = [];
+dic = {};
+
+
+patronART = re.compile('\w[0-9]');
+patronART2 = re.compile('category page-link \w');
+listaArticulos = soup.find_all("article",attrs={"dtm-name":patronART});
+
+for art in listaArticulos:
+    info_Categoria =art.find('a', class_=patronART2);
+    info_titulo =  art.find('a', class_='title page-link');
+    info_fecha = art.find('span', class_ = 'published-at');
+    if(info_Categoria != None and info_titulo!= None):
+        dic = {
+        #'Fecha': info_fecha.string,
+        'Url_categoria': info_Categoria.get('href'),
+        'Categoria': info_Categoria.string,
+        'Url_Noticia': info_titulo.get('href'),
+        'Titulo':info_titulo.string,
+        };
+        print(dic)
+
+
+
+
+
 
